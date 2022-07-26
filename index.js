a53db0275cb04e3d7af880080c42c463a73d6e34
Samuel Png <samuelpng7@gmail.com>
2022-07-26 16:00:29 +0000

    Changed server port

--- index.js
+++ index.js
@@ -368,9 +368,9 @@ async function main() {
 main();
 
 //START SERVER
-app.listen(8000, () => {
-  console.log("Server has started")
-})
-// app.listen(process.env.PORT, () => {
+// app.listen(8000, () => {
 //   console.log("Server has started")
 // })
+app.listen(process.env.PORT, () => {
+  console.log("Server has started")
+})
